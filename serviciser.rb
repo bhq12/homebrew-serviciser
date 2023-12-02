5e9073057d92a6d22720d7d11c7cc301ab06e8e9
Brook Q <bhq12@users.noreply.github.com>
2023-12-03 08:41:45 +1000

    Update serviciser.rb

--- serviciser.rb
+++ serviciser.rb
@@ -1,8 +1,8 @@
 class Serviciser < Formula
     desc "CLI Tool to initialise new service projects"
     homepage "https://github.com/bhq12/serviciser"
-    url "https://github.com/bhq12/serviciser/releases/download/publish-to-brew20231201T094854/cli"
-    sha256 ""
+    url "https://github.com/bhq12/serviciser/releases/download/refs/heads/publish-to-brew/cli.gz"
+    sha256 "9159ede2187e9c0836ad0692d6cfabe48caea0cdf747d3c949d3f9b7ce8fa17c"
     version "v0.0.1"
   
     bottle :unneeded
