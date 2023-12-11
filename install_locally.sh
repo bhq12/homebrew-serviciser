a609965cefe150050b73bc9c73c3a7d94d16cdbc
Brook Q <bhq12@users.noreply.github.com>
2023-12-11 14:40:53 +1000

    Set up python dependencies and point at release URL, add local install script for debugging

---
+++ install_locally.sh
@@ -0,0 +1,2 @@
+HOMEBREW_NO_INSTALL_FROM_API=1 brew reinstall --build-from-source --verbose --debug serviciser.rb
+#HOMEBREW_NO_INSTALL_FROM_API=1 brew reinstall --build-from-source --verbose --debug brook_hello.rb
